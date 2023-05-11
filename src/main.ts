5662f1d742b9f21d42d2d04614a436cdff335f27
Mazhorova <mazhorova1987@mail.ru>
2023-05-11 19:07:19 +0300

    vite

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,5 @@
+// Импорт делают чтобы не прописывать в head link js
 import './style.css'
 import './style.scss'
+import 'lite-youtube-embed/src/lite-yt-embed.css'
+import 'lite-youtube-embed/src/lite-yt-embed.js'
